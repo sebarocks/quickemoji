3d91fbf90f0f77c73ef9756118b4ed9b787693b0
sebarocks <sebastian_orrego_a@outlook.com>
2023-01-09 12:30:05 -0300

    config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '',
+  build:{
+    outDir: 'extension/popup/',
+    assetsDir:'./',
+
+    rollupOptions: {
+      output:{
+        entryFileNames: "[name].js",
+      }
+    }
+  }
 })
